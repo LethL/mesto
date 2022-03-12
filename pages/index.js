a48bd2c2e7f7a7b62fcff07da7586e72683de318
Leth4L <carribean_08@mail.ru>
2022-03-12 18:54:43 +0300

    feat: add function to add place cards from modal window

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
-let popupOpenBtn = document.querySelector('.profile__edit-button')
+let editProfileBtn = document.querySelector('.profile__edit-button');
+let addPlaceBtn = document.querySelector('.profile__add-button');
 let popupCloseBtn = document.querySelector('.popup__close-btn');
 let popup = document.querySelector('.popup');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 let profileTitleInput = document.querySelector('#title');
 let profileSubtitleInput = document.querySelector('#subtitle');
-let form = document.querySelector('.popup__form');
+let addProfileForm = document.querySelector('#edit-profile-form');
+let addPlaceForm = document.querySelector('#add-place-form');
 let likeBtns = document.querySelectorAll('.element__like');
+const placeNameInput = document.querySelector('#name-input');
+const placeImageInput = document.querySelector('#image-input');
+const cardsList = document.querySelector('.elements');
 const initialCards = [
     {
       name: 'Архыз',
@@ -41,9 +46,18 @@ function closeForm() {
 function openForm() {
     editInputValue();
     popup.classList.add('popup_opened');
+    addPlaceForm.classList.add('popup__form_disabled');
+    addProfileForm.classList.remove('popup__form_disabled');
 }
 
-popupOpenBtn.addEventListener('click', openForm)
+function openAddPlaceForm() {
+  popup.classList.add('popup_opened');
+  addProfileForm.classList.add('popup__form_disabled');
+  addPlaceForm.classList.remove('popup__form_disabled');
+}
+
+editProfileBtn.addEventListener('click', openForm)
+addPlaceBtn.addEventListener('click', openAddPlaceForm)
 
 popupCloseBtn.addEventListener('click', closeForm)
 
@@ -59,20 +73,18 @@ function editProfileText() {
 }
 
 
-form.addEventListener('submit', e => {
+addProfileForm.addEventListener('submit', e => {
     e.preventDefault();
     editProfileText();
     closeForm();
 })
-
-const cardsList = document.querySelector('.elements')
 
 function renderCards(link, name) {
     const cardTemplate = document.querySelector('#template').content;
     const card = cardTemplate.querySelector('.element').cloneNode(true);
     card.querySelector('.element__image').src = link;
     card.querySelector('.element__info').querySelector('.element__title').textContent = name;
-    cardsList.append(card);
+    cardsList.prepend(card);
 }
 
 initialCards.map(card => {
@@ -83,4 +95,16 @@ likeBtns.forEach(e => {
     e.addEventListener('click', () => {
         e.classList.toggle('like_active')
     })
-});
+})
+
+addPlaceForm.addEventListener('submit', addPlace)
+
+function addPlace(event) {
+  event.preventDefault()
+  const link = placeImageInput.value
+  const name = placeNameInput.value
+  renderCards(link, name)
+  placeImageInput.value = ''
+  placeNameInput.value = ''
+  closeForm()
+}
